6156886237bbd54bf97249e3047dba34ad8f7e27
Hazem Gharib <hazem.gharb.8@gmail.com>
2018-06-10 15:39:58 +0200

    Adding swagger configuration

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NJsonSchema;
+using NSwag.AspNetCore;
+using System.Reflection;
 
 namespace dotnet_core_apis
 {
@@ -33,6 +36,27 @@ namespace dotnet_core_apis
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable the Swagger UI middleware and the Swagger generator
+            app.UseSwaggerUi(typeof(Startup).GetTypeInfo().Assembly, settings =>
+            {
+                settings.GeneratorSettings.DefaultPropertyNameHandling = 
+                    PropertyNameHandling.CamelCase;
+
+                settings.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Dot Net Core APIs";
+                    document.Info.Description = "Web APIs made by ASP.Net Core 2 for an integration with react\\redux application";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.SwaggerContact
+                    {
+                        Name = "Hazem Gharib",
+                        Email = "hazem.gharib.8@gmail.com",
+                        Url = string.Empty
+                    };
+                };
+            });
 
             app.UseMvc();
         }
