2d583e8f822305f09d57be81031845e753974980
Hazem Gharib <hazem.gharb.8@gmail.com>
2018-06-12 03:43:08 +0200

    Implementing POST, PUT & DELETE APIs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dotnet_core_apis.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace dotnet_core_apis.Controllers
 {
@@ -36,20 +38,69 @@ namespace dotnet_core_apis.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public int Post([FromBody]string value)
         {
+           EntityEntry<Values> result = null;
+           try
+           {
+                using (var ctx = new core2apiv1Context())
+                {
+                    result = ctx.Values.Add( new Values{Id = Guid.NewGuid(),Value = value});
+                    ctx.SaveChanges();
+                }
+                return StatusCodes.Status200OK;
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine(ex.Message);
+               Console.WriteLine(result);
+               return StatusCodes.Status500InternalServerError;
+           }
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public int Put(Guid id, [FromBody]string value)
         {
+           Values result = null;
+           try
+           {
+                using (var ctx = new core2apiv1Context())
+                {
+                    result = ctx.Values.SingleOrDefault(x => x.Id == id);
+                    result.Value = value;
+                    ctx.SaveChanges();
+                }
+                return StatusCodes.Status200OK;
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine(ex.Message);
+               Console.WriteLine(result);
+               return StatusCodes.Status500InternalServerError;
+           }
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public int Delete(Guid id)
         {
+            EntityEntry<Values> result = null;
+           try
+           {
+                using (var ctx = new core2apiv1Context())
+                {
+                    result = ctx.Values.Remove(ctx.Values.SingleOrDefault(x => x.Id == id));
+                    ctx.SaveChanges();
+                }
+                return StatusCodes.Status200OK;
+           }
+           catch (Exception ex)
+           {
+               Console.WriteLine(ex.Message);
+               Console.WriteLine(result);
+               return StatusCodes.Status500InternalServerError;
+           }
         }
     }
 }
